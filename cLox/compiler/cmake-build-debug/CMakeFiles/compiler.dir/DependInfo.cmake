de699eadc5730d5288832ac0ac27cc5c249524d0
chui <1229059439@qq.com>
2021-04-11 10:53:25 +0800

    gitignore

---
+++ cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "F:/Github/Compiler/cLox/compiler/chunk.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/chunk.c.obj"
+  "F:/Github/Compiler/cLox/compiler/compiler.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/compiler.c.obj"
+  "F:/Github/Compiler/cLox/compiler/debug.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/debug.c.obj"
+  "F:/Github/Compiler/cLox/compiler/main.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/main.c.obj"
+  "F:/Github/Compiler/cLox/compiler/memory.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/memory.c.obj"
+  "F:/Github/Compiler/cLox/compiler/object.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/object.c.obj"
+  "F:/Github/Compiler/cLox/compiler/scanner.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/scanner.c.obj"
+  "F:/Github/Compiler/cLox/compiler/table.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/table.c.obj"
+  "F:/Github/Compiler/cLox/compiler/value.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/value.c.obj"
+  "F:/Github/Compiler/cLox/compiler/vm.c" "F:/Github/Compiler/cLox/compiler/cmake-build-debug/CMakeFiles/compiler.dir/vm.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
